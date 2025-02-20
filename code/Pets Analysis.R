8641ea3b8ae31e1e0e046fa321b1803d764cbf48
Dr Prabath jayatissa <prabathjayatissa1905@gmail.com>
2025-02-20 22:27:35 +0100

    1

--- code/Pets Analysis.R
+++ code/Pets Analysis.R
@@ -15,7 +15,8 @@ data_dogs <- data %>%
   dplyr::filter(pet=="dog")  
 data_dogs
 
-#Chart the Dogs weight against the Score 
+# Chart the Dogs weight against the Score - Scatter Plot, Box-Plot, Frequency-Polygon, Histogram
+
 ggplot(data_dogs, aes(x=weight, y=score))+
   geom_point(col="Red", size =2)+
   labs(x="Weight of Dogs",
@@ -25,10 +26,29 @@ ggplot(data_dogs, aes(x=weight, y=score))+
 
 head(data_dogs)
 ggplot(data_dogs, aes(x=country, y=age))+
-  geom_boxplot(col="Red",fill= "lightblue")+
+  geom_boxplot(col="Red",fill= "lightblue", size = 0.5)+
   labs(x="Weight of Dogs",
        y="Score of the Dogs",
        title="Performance of Dogs",
        caption = "Performance of Dogs in UK during 1891")
 
+
+ggplot(data_dogs, aes(x=score))+
+  geom_freqpoly(binwidth= 2, col="red")+
+  geom_histogram(col="lightblue")+
+  labs(y="Number of Dogs",
+       x="Score of the Dogs",
+       title="Performance of Dogs",
+       caption = "Performance of Dogs in UK during 1891")
+
+
+library(tidyverse)
+library(dslabs)  
+data(murders)
+
+murders %>%
+  ggplot(aes(population,total,label=abb, color=region))+
+  geom_label()
+       
+           
   
